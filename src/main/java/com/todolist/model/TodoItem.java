cfe433573a8d1079fdf90e03a841ffc26d1b2d8e
Loganathan Sellappa <loganathan.sellappa@logmein.com>
2019-03-31 14:49:58 +0200

    Lombok added for model

--- src/main/java/com/todolist/model/TodoItem.java
+++ src/main/java/com/todolist/model/TodoItem.java
@@ -1,7 +1,17 @@
 package com.todolist.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import java.time.LocalDate;
 
+//Lombok will generate getter, setter, equals, hashcode, can equal
+// By default lombnok generates hashCode and Equals for all fields to
+// avoid that we can add another annotation EqualsAndHashCode with field add in paramerter
+// We can also generate EqualsAndHashCode for multiple fields using paranthesis
+// @EqualsAndHashCode(of = {"id", "details"})
+@Data
+@EqualsAndHashCode(of = "id")
 public class TodoItem {
 
 	//== fields
@@ -15,64 +25,5 @@ public class TodoItem {
 		this.title = title;
 		this.details = details;
 		this.deadline = deadline;
-	}
-
-	public String getTitle () {
-		return title;
-	}
-
-	public void setTitle (String title) {
-		this.title = title;
-	}
-
-	public String getDetails () {
-		return details;
-	}
-
-	public void setDetails (String details) {
-		this.details = details;
-	}
-
-	public LocalDate getDeadline () {
-		return deadline;
-	}
-
-	public void setDeadline (LocalDate deadline) {
-		this.deadline = deadline;
-	}
-
-	public int getId () {
-		return id;
-	}
-
-	public void setId (int id) {
-		this.id = id;
-	}
-
-	@Override
-	public boolean equals (Object o) {
-		if (this == o)
-			return true;
-		if (!(o instanceof TodoItem))
-			return false;
-
-		TodoItem todoItem = (TodoItem) o;
-
-		return id == todoItem.id;
-	}
-
-	@Override
-	public int hashCode () {
-		return id;
-	}
-
-	@Override
-	public String toString () {
-		return "TodoItem{" +
-			       "id=" + id +
-			       ", title='" + title + '\'' +
-			       ", details='" + details + '\'' +
-			       ", deadline=" + deadline +
-			       '}';
 	}
 }
