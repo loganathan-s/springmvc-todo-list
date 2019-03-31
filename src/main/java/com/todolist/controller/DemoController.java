b003a40251c792782996ec2cc383c410c977f56f
Loganathan Sellappa <loganathan.sellappa@logmein.com>
2019-03-31 13:45:34 +0200

    Welcome JSP url added

--- src/main/java/com/todolist/controller/DemoController.java
+++ src/main/java/com/todolist/controller/DemoController.java
@@ -7,10 +7,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
-	
+
 	@ResponseBody
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello";
+	}
+
+	// Ve dont need to enter the web-inf or whatever the file path path
+	// of the view file since view resolver will do that for us
+	// http://localhost:8080/todo-list/welcome
+	@GetMapping("welcome")
+	public String welcome() {
+		//Name of the JSP file
+		return "welcome";
 	}
 }
---
+++ src/main/webapp/WEB-INF/view/welcome.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: loganathans
+  Date: 2019-03-31
+  Time: 13:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+    ....Started App....
+</body>
+</html>
