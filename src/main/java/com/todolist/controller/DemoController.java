259ba1808656bfd41e902a461d694506de767dd0
Loganathan Sellappa <loganathan.sellappa@logmein.com>
2019-03-31 13:24:54 +0200

    Demo request implemented

---
+++ src/main/java/com/todolist/controller/DemoController.java
@@ -0,0 +1,16 @@
+package com.todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class DemoController {
+	
+	@ResponseBody
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello";
+	}
+}
